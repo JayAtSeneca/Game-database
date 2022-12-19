b4db17e93f4cf00e454457af44fc7ff9c0dd32cd
JayAtSeneca <jpchaudhari@myseneca.ca>
2022-12-18 23:05:31 -0500

    created game-tabs and details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,12 +14,16 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { HomeComponent } from './components/home/home.component';
+import { GameTabsComponent } from './components/game-tabs/game-tabs.component';
+import { DetailsComponent } from './components/details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
-    HomeComponent
+    HomeComponent,
+    GameTabsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
